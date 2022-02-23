ad4a4a63d4daaf68c2d046e915b080355cbf2558
Antonio Riccelli <anton.riccelli@gmail.com>
2022-02-23 15:52:34 +0000

    Added a get comments by Event Id route

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
-import {getComments, getCommentsByUserId, createComment} from "../controller/commentsController.js";
+import {getComments, getCommentsByUserId, getCommentsByEventId,createComment} from "../controller/commentsController.js";
 
 
 const router = express.Router()
 
 router.get('/', getComments);
 router.get("/:id", getCommentsByUserId);
+router.get("/event/:id", getCommentsByEventId);
 router.post('/', createComment);
-
 
 export default router;
   
